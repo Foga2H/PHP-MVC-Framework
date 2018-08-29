a0ff15aa6542182969ca29e5f2cc92a11b9a53c3
foga <fatkullin@deck.lc>
2018-08-29 16:54:11 +0300

    Lesson #28 - Remove query string variables...

---
+++ App/Controllers/Home.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controllers;
+
+/**
+ * Home Controller
+ * @package App\Controllers
+ */
+class Home
+{
+    /**
+     * Show the index page
+     *
+     * @return void
+     */
+    public function index()
+    {
+        echo 'Hello from the index action in the Home controller!';
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Front Controller
+ */
+
+/**
+ * Autoloader
+ */
+spl_autoload_register(function($class) {
+    $root = dirname(__DIR__); // get the parent directory
+    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
+
+    if(is_readable($file)) {
+        require $root . '/' . str_replace('\\', '/', $class) . '.php';
+    }
+});
+
+/**
+ * Routing
+ */
+require '../Core/Router.php';
+
+$router = new \Core\Router();
+
+// Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('{controller}/{action}');
+$router->add('{controller}/{id:\d+}/{action}');
+
+$router->dispatch($_SERVER['QUERY_STRING']);
